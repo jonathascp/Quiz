7a93daa5cb57daa2234bef56af43d21f1cc35fce
Jonathas dos Anjos <jonathas_rj_10@hotmail.com>
2026-02-11 22:45:16 -0300

    Ajuste nas perguntas , ajuste no tamanho de perguntas

--- src/app/fimDeJogo/page.js
+++ src/app/fimDeJogo/page.js
@@ -3,9 +3,14 @@ import styles from "./styles.module.css";
 import { useRouter } from "next/navigation";
 import perguntas from "../perguntas.json";
 import { useEffect, useState } from "react";
+import { useTema } from "../Context/TemasContext";
+
+
 export default function fimDeJogo() {
-    const temaSelecionado = perguntas.temas.find((t) => t.tema === "Mateus");
+    const { tema } = useTema();
+    const temaSelecionado = perguntas.temas.find((t) => t.tema === tema);
     const [pontos,setPontos] = useState(0);
+    console.log(temaSelecionado);
     useEffect(() => {
         const novosPontos = sessionStorage.getItem("acertos");
         if(novosPontos)
@@ -27,11 +32,11 @@ export default function fimDeJogo() {
         <div className={`${styles.container} d-flex flex-column justify-content-center align-items-center w-100`}>
             <div className={styles.painel}>
                 <h1 className={styles.titulo}>Fim de jogo.</h1>
-                <p className={styles.pontuacao}>Você acerto {pontos} de {temaSelecionado.perguntas.length}</p>
+                <p className={styles.pontuacao}>Você acerto {pontos} de {temaSelecionado.perguntas.length}.</p>
                 <button className={styles.botaoVoltar} onClick={() => {
-                    router.push("/quests")
+                    router.push("/tema")
                     setPontos(0);
-                } }>voltar ao jogo</button>
+                } }>Escolher novo tema</button>
             </div>
         </div>
 
--- src/app/tema/page.js
+++ src/app/tema/page.js
@@ -1,10 +1,11 @@
 import Botao from "../Components/Botao_tema/BotaoTema.js";
+import styles from "./styles.module.css";
 export default function Temas() {
     
     return (
         <div className={`w-100 d-flex flex-column justify-content-center align-items-center`}>
-            <h1>Escolha um tema.</h1>
-            <div>
+            <h1 className={styles.titulo}>Escolha um tema.</h1>
+            <div className={`d-flex flex-column justify-content-center align-items-center gap-3`}>
                 <Botao tema={"Marcos"} descricao={"Marcos"}/>
                 <Botao tema={"Mateus"} descricao={"Mateus"}/>
             </div>
